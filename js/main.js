1faee9193f1dad5c67961d39dbd10f3a1fefa624
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2020-12-31 19:48:20 +0200

    added preloader, vendor prefixes to a styles and check HTML and CSS files on the presence of any issue via validator

--- js/main.js
+++ js/main.js
@@ -71,4 +71,14 @@ const getIpAdress = () => {
 
 };
 
+const runPreloader = () => {
+   const preloader = document.querySelector(".preloader-container");
+   preloader.classList.add("opacity-0");
+
+   setTimeout(() => {
+      preloader.style.display = "none";
+   }, 1000);
+};
+
 getIpAdress();
+runPreloader();
